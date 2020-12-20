bad081a3127b6ef36bd2de899573d21ba6b576f9
Victor Almeida <jvassp@gmail.com>
2020-12-20 02:13:39 -0300

    teste boy

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,6 @@
 import '../css/style.css'
 import '../css/pitagoras.css'
+import '../css/bhaskara.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 export default function App({ Component, pageProps }) {
