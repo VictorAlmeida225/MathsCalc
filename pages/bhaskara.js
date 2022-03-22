46edd4dcfec6f7c7689bbd924ee332aeff8c177d
João Almeida <jvassp@gmail.com>
2022-03-22 20:55:57 -0300

    first

--- pages/bhaskara.js
+++ pages/bhaskara.js
@@ -1,6 +1,26 @@
 import React from 'react';
 import Helmet from 'react-helmet';
-import { Button, Col, Row, Container, Table, Modal, Navbar, Nav } from 'react-bootstrap';
+import { Alert, OverlayTrigger, Button, Col, Row, Container, Modal, Navbar, Nav, InputGroup, Form, Image, Tooltip } from 'react-bootstrap';
+
+class ButtonSubmit extends React.Component {
+    state = {
+        Hovered: false,
+    }
+
+    render() {
+        return (
+            <div>
+                <Button
+                    className="buttonbtn"
+                    variant={this.state.Hovered ? "primary" : "outline-primary"}
+                    onMouseEnter={() => this.setState({ Hovered: true })}
+                    onMouseLeave={() => this.setState({ Hovered: false })}
+                    onClick={Calculate}>
+                    Calcular</Button>
+            </div>
+        )
+    }
+}
 
 function App(){
     return (
@@ -29,11 +49,48 @@ function App(){
             </Nav>
             </Navbar.Collapse>
         </Navbar>
-        <Container>
-
-        </Container>
+            <Container>
+                <Row className='RowInp'>
+                    <Col md={12} xl={2}><h3 id="lbl1" className="unselective">A:</h3></Col>
+                    <Col md={12} xl={10}>
+                        <InputGroup className="mb-3">
+                            <Form.Control type="number" id="A" />
+                        </InputGroup>
+                    </Col>
+                    <Col md={12} xl={2}><h3 id="lbl1" className="unselective">B:</h3></Col>
+                    <Col md={12} xl={10}>
+                        <InputGroup className="mb-3">
+                            <Form.Control type="number" id="B" />
+                        </InputGroup>
+                    </Col>
+                    <Col md={12} xl={2}><h3 id="lbl1" className="unselective">C:</h3></Col>
+                    <Col md={12} xl={10}>
+                        <InputGroup className="mb-3">
+                            <Form.Control type="number" id="C" />
+                        </InputGroup>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col xs={12} className="SubCol"><ButtonSubmit /></Col>
+                </Row>
+            </Container>
         </div>
     )
 }
 
+function Calculate(){
+    var A = document.getElementById('A').value;
+    var B = document.getElementById('B').value;
+    var C = document.getElementById('C').value;
+
+    if (!A) {
+        document.getElementById("A").placeholder = "Nenhum número digitado!";
+    }
+    if (!B) {
+        document.getElementById("B").placeholder = "Nenhum número digitado!";
+    }
+    if (!C) {
+        document.getElementById("C").placeholder = "Nenhum número digitado!";
+    }
+}
 export default App
